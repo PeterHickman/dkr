52eb3928415799166b4a972d1c3bce55d5cd8eef
Peter Hickman <peterhickman386@googlemail.com>
2024-07-06 15:43:51 +0100

    go version of the install

--- install.sh
+++ install.sh
@@ -1,20 +1,12 @@
 #!/bin/sh
 
-if ! [ "$(id -u)" = 0 ]; then
-  echo 'You must be root to do this.' 1>&2
-  exit 1
-fi
+BINARY='/usr/local/bin'
 
-inst() {
-  echo "Installing $1"
-  install -g $ROOT -o $ROOT -m 0755 $1 /usr/local/bin/$1
-}
+echo "Building dkr"
+go build dkr.go
 
-##
-# When installing we would install as root, however on
-# BSD based systems (such as OSX) this is not a valid
-# group / user for install so we use the numeric value
-##
-ROOT="0"
+echo "Installing dkr to $BINARY"
+install -v dkr $BINARY
 
-inst dkr
+echo "Removing the build"
+rm dkr
